93b71c841010a4c127611839ab81b515876f8152
Artem Belov <inctnce@icloud.com>
2020-10-31 14:28:41 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.listen(8000, () => {
+  console.log(`Server is running on port: 8000`);
+});
