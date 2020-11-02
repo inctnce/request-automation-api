d3c2165ad4f9d4f8e4913171c147afd09f11d678
Artem Belov <inctnce@icloud.com>
2020-11-02 18:25:52 +0300

    .

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,10 @@ app.use(express.json());
 app.use(cors());
 app.use("/", user_routes);
 
+app.get("/", (_req, res) => {
+  res.send("<h1>Hello</h1>");
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
