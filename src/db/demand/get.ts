e718a5a1339660ea052360b972cd5c609530d34e
Artem Belov <inctnce@icloud.com>
2020-11-28 19:06:43 +0300

    UPDATE get methods for demands and products

--- src/db/demand/get.ts
+++ src/db/demand/get.ts
@@ -1,13 +1,16 @@
 import Demand from "src/types/Demand";
 import pool from "../pool";
 
-export default function get(): Promise<Demand[]> {
+export default function get(creator_id?: string): Promise<Demand[]> {
   return new Promise((resolve, reject) => {
-    pool.query(`SELECT * FROM demand`, (err, results) => {
-      if (err) {
-        return reject(err);
+    pool.query(
+      creator_id ? `SELECT * FROM demand WHERE creator_id="${creator_id}"` : `SELECT * FROM demand`,
+      (err, results) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(results);
       }
-      return resolve(results);
-    });
+    );
   });
 }
--- src/db/product/get.ts
+++ src/db/product/get.ts
@@ -1,13 +1,16 @@
 import Product from "src/types/Product";
 import pool from "../pool";
 
-export default function get(): Promise<Product[]> {
+export default function get(category_id?: string): Promise<Product[]> {
   return new Promise((resolve, reject) => {
-    pool.query(`SELECT * FROM product`, (err, results) => {
-      if (err) {
-        return reject(err);
+    pool.query(
+      category_id ? `SELECT * FROM product WHERE category_id="${category_id}"` : `SELECT * FROM product`,
+      (err, results) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(results);
       }
-      return resolve(results);
-    });
+    );
   });
 }
