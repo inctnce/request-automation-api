8fa75636eb618e3899f59efe916fbdb8e8269548
Artem Belov <inctnce@icloud.com>
2020-12-10 14:24:02 +0600

    конфигурация бд в .env

--- src/db/pool.ts
+++ src/db/pool.ts
@@ -1,9 +1,9 @@
 import mysql from "mysql";
 
 export default mysql.createPool({
-  password: "d95c5c5f",
-  user: "b34c95e74e8730",
-  database: "heroku_37ee2feeeafbb67",
-  host: "eu-cdbr-west-03.cleardb.net",
-  port: 3306,
+  password: process.env.password,
+  user: process.env.user,
+  database: process.env.database,
+  host: process.env.host,
+  port: Number(String(process.env.port)),
 });
