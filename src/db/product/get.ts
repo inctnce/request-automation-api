7d267cf07544821a1226a30495e7ba8dce946501
Artem Belov <inctnce@icloud.com>
2020-12-10 14:24:10 +0600

    рефакторинг

--- src/db/product/get.ts
+++ src/db/product/get.ts
@@ -15,9 +15,7 @@ function formulateRequest(id?: string, key?: string): string {
 export default function get(id?: string, key?: string): Promise<Product[]> {
   return new Promise((resolve, reject) => {
     pool.query(formulateRequest(id, key), (err, results) => {
-      if (err) {
-        return reject(err);
-      }
+      if (err) return reject(err);
       return resolve(results);
     });
   });
